f85707ef89ca890536a9e23b0b688d9ae9874c05
AngusBD <97964789+AngusBD@users.noreply.github.com>
2022-11-23 17:04:46 +0800

    Add files via upload

---
+++ 1101625.cpp
@@ -0,0 +1,164 @@
+﻿#include <iostream>
+using namespace std;
+
+class LinkedList;
+class ListNode{
+private:
+    int data;
+    ListNode* next;
+    ListNode* prev;
+public:
+    ListNode() :prev(NULL), next(NULL), data(0) { };
+    ListNode(int a) :prev(NULL), next(NULL), data(a) { };
+    friend class LinkedList;
+};
+
+class LinkedList{
+private:
+    ListNode* first;
+public:
+    LinkedList() :first(0) {};
+    void PrintList();
+    void Insert(int x);
+    void Delete(int x);
+    void Search(int x);			
+    void Clear();           
+};
+
+void LinkedList::PrintList(){
+
+    if (first == 0){
+        cout << "List is empty.\n";
+        return;
+    }
+
+    ListNode* current = first; 
+    while (current != 0){
+        cout << current->data << " ";
+        current = current->next;
+    }
+    cout << endl;
+}
+
+void LinkedList::Insert(int x){
+
+    ListNode* newNode = new ListNode(x); 
+
+    if (first == 0){// 1.Doubly Linked List是空的
+        first = newNode;
+        return;
+    }
+
+    if (first->data > x/*如果first指到的node值比新增的值大的話*/){//2.要插入的Node的值是最小的(要放在最前面)
+        newNode->next = first;
+        first->prev = newNode;
+        first = newNode;
+        return;
+    }
+
+    ListNode* current = first;
+
+    while (current->next != 0 && current->next->data < x){
+        current = current->next;
+    }
+
+    if (current->next == 0/*如果current指到的node值後面沒有node*/){//3.2 要新增的node剛好在list的最後
+        current->next = newNode;
+        newNode->prev = current;
+        return;
+    }
+    else{//3.1 node不新增在first, 也不在最後一個
+        newNode->prev = current;
+        newNode->next = current->next;
+        current->next->prev = newNode;
+        current->next = newNode;
+        return;
+    }
+
+}
+
+void LinkedList::Delete(int x){
+    //Traversal, 檢查list是否為empty或是否有要刪的node
+    ListNode* current = first, * previous = 0;
+
+    while (current != 0 && current->data != x){
+        previous = current;
+        current = current->next;
+    }
+    if (current == 0){
+        cout << "There is no " << x << " in list.\n";
+    }
+    else if (current == first){
+        //1. 刪除的node剛好在list的開頭：要先把first指到下一個node, 再把要刪除的node刪掉
+        first = current->next;
+        delete current;
+        current = 0;
+    }
+    else if (current->next == 0){
+        //2. 刪除的node剛好在list的最後：要先把previous指到下一個node, 再把要刪除的node刪掉
+        previous->next = current->next;
+        delete current;
+        current = 0;
+    }
+    else{
+        //3. 其餘情況(list中有欲刪除的node, 且node不為first, 也不為最後一個)：
+        //把要刪掉的上一個node(所以你前面可以先有一個指標指到上一個node)指到要刪掉的下一個node,
+        //接著把下一個node的prev指到上一個node, 再把要刪除的node刪掉 
+        previous->next = current->next;
+        current->next->prev = previous;
+        delete current;
+        current = 0;
+    }
+}
+
+void LinkedList::Search(int x){
+    ListNode* current = first;
+    int pos = 0;
+
+    while (current != 0 && current->data != x){
+        current = current->next;
+        pos++;
+    }
+    if (current == 0){
+        cout << "There is no " << x << " in list.\n";
+    }
+    else{
+        cout << "There is " << x << " in list.At the " << pos + 1 << " place.\n";
+    }
+}
+
+void LinkedList::Clear(){
+
+    while (first != 0){ // Traversal
+        ListNode* current = first;
+        first = first->next;
+        delete current;
+        current = 0;
+    }
+}
+
+int main(void){
+
+    LinkedList list;     // 建立LinkedList的object
+    list.PrintList();    // 目前list是空的
+    list.Delete(4);      // list是空的, 沒有4
+    list.Insert(5);   // list: 5
+    list.Insert(3);   // list: 3 5
+    list.Search(5);   // 印出:There is 5 in list.At the 2 place.
+    // list.PrintList();
+    list.Insert(9);  // list: 3 5 9
+    list.Insert(4);   // list: 3 4 5 9
+    list.PrintList();    // 印出:  3 4 5 9
+    list.Search(1);   // 印出:There is no 1 in list.
+    list.Delete(3);      // list: 4 5 9
+    list.PrintList();    // 印出: 4 5 9
+    list.Delete(7);		// list沒有7
+    list.Insert(8);  // list: 4 5 8 9
+    list.Insert(2);  // list: 2 4 5 8 9
+    list.PrintList();    // 印出:  2 4 5 8 9
+    list.Search(8);   // 印出:There is 8 in list.At the 4 place.
+    list.Clear();        // 清空list
+    list.PrintList();    // 印出: List is empty.*/
+
+    return 0;
+}
---
+++ 1101625_1101658.cpp
@@ -0,0 +1,146 @@
+﻿// 1101625林暉恩
+// 1101658楊嘉冠
+
+#include <iostream>
+using namespace std;
+
+int capacity = 10;
+
+void output(int *int_arr){
+	for (int i = 0; i < capacity; i++){
+		cout << int_arr[i] << '\t';
+	}
+	cout << endl;
+}
+
+void initialize(int *int_arr, int *original_arr){
+	for (int i = 0; i < capacity; i++){
+		int_arr[i] = original_arr[i];
+	}
+	cout << endl;
+}
+
+void swap(int *num1Ptr, int *num2Ptr){
+	int temp;
+	temp = *num1Ptr;
+	*num1Ptr = *num2Ptr;
+	*num2Ptr = temp;
+	// *num1Ptr ^= *num2Ptr;
+	// *num2Ptr ^= *num1Ptr;
+	// *num1Ptr ^= *num2Ptr;
+}
+
+void BubbleSortAscending(int *int_arr){
+	for (int i = 0; i < capacity - 1; i++){
+		for (int j = 0; j < capacity - 1 - i; j++){
+			if (int_arr[j] > int_arr[j + 1]){
+				swap(&int_arr[j], &int_arr[j + 1]);
+			}
+		}
+		output(int_arr);
+	}
+}
+
+void BubbleSortDecending(int *int_arr){
+	for (int i = 0; i < capacity - 1; i++){
+		for (int j = 0; j < capacity - 1 - i; j++){
+			if (int_arr[j] < int_arr[j + 1]){
+				swap(&int_arr[j], &int_arr[j + 1]);
+			}
+		}
+		output(int_arr);
+	}
+}
+
+void SelectionSortAscending(int *int_arr){
+	for (int i = 0; i < capacity - 1; i++){
+		int min = i;
+		for (int j = i + 1; j < capacity; j++){
+			if (int_arr[j] < int_arr[min]){
+				min = j;
+			}
+		}
+		swap(&int_arr[min], &int_arr[i]);
+		output(int_arr);
+	}
+}
+
+void SelectionSortDecending(int *int_arr){
+	for (int i = 0; i < capacity - 1; i++){
+		int max = i;
+		for (int j = i + 1; j < capacity; j++){
+			if (int_arr[j] > int_arr[max]){
+				max = j;
+			}
+		}
+		swap(&int_arr[max], &int_arr[i]);
+		output(int_arr);
+	}
+}
+
+void InsertionSortAscending(int *int_arr){
+	for (int i = 1; i < capacity; i++){
+		int key = int_arr[i];
+		int j = i - 1;
+		while (key < int_arr[j] && j >= 0){
+			int_arr[j + 1] = int_arr[j];
+			j--;
+		}
+		int_arr[j + 1] = key;
+		output(int_arr);
+	}
+}
+
+void InsertionSortDecending(int *int_arr){
+	for (int i = 1; i < capacity; i++){
+		int key = int_arr[i];
+		int j = i - 1;
+		while (key > int_arr[j] && j >= 0){
+			int_arr[j + 1] = int_arr[j];
+			j--;
+		}
+		int_arr[j + 1] = key;
+		output(int_arr);
+	}
+}
+
+int main(){
+	srand(time(NULL));
+	int int_arr[10], original_arr[10];
+	// int int_arr[10], original_arr[10] = {0,1,2,3,4,5,6,7,8,9};
+	// int int_arr[10], original_arr[10] = {9,8,7,6,5,4,3,2,1,0};
+	cout << "Original Array:" << endl;
+	for (int i = 0; i < capacity; i++){
+		original_arr[i] = rand() % 20 + 1;
+		int_arr[i] = original_arr[i];
+	}
+	output(int_arr);
+
+	cout << "BubbleSort(Ascending):" << endl;
+	BubbleSortAscending(int_arr);
+	initialize(int_arr, original_arr);
+
+	cout << "BubbleSort(Decending):" << endl;
+	BubbleSortDecending(int_arr);
+	initialize(int_arr, original_arr);
+
+	cout << endl;
+
+	cout << "SelectionSort(Ascending):" << endl;
+	SelectionSortAscending(int_arr);
+	initialize(int_arr, original_arr);
+
+	cout << "SelectionSort(Decending):" << endl;
+	SelectionSortDecending(int_arr);
+	initialize(int_arr, original_arr);
+
+	cout << endl;
+
+	cout << "InsertionSort(Ascending):" << endl;
+	InsertionSortAscending(int_arr);
+	initialize(int_arr, original_arr);
+
+	cout << "InsertionSort(Decending):" << endl;
+	InsertionSortDecending(int_arr);
+	initialize(int_arr, original_arr);
+}
